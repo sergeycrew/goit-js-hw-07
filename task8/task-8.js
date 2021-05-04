627c28cba9185bbe19b5e39470e9890fc36c7e87
Sergey Crew <sergeycrew.bs@gmail.com>
2021-05-03 19:51:48 -0700

    Update task-8.js

--- task8/task-8.js
+++ task8/task-8.js
@@ -12,13 +12,7 @@
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
 
-// const ref = {
-//     input: document.querySelector('#controls input'),
-//     create: document.querySelector('#controls button[data-action="render"]'),
-//     clear: document.querySelector('#controls button[data-action="destroy"]'),
-//     boxesShow: document.querySelector('#boxes')
-// }
-// const randomHexColor = () => Math.floor(Math.random()*16777215).toString(16);
+
 
 
 
@@ -62,29 +56,4 @@ const createBtnEl = document.querySelector(
     inputEl.value = "";
   }
 
-
-
-// function createBoxes(amount) {
-    
-//     let squares = '';
-//     for (let i = 0; i < amount; i += 1){
-//         squares += `<div id="box"style="width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color: #${randomHexColor()}; margin: 5px;"></div>`
-//     }
-//         ref.boxes.insertAdjacentHTML('beforeend', squares);
-//         ref.input.value = '';
-// }
-
-// function createBoxesOnEnter(e) {
-//    if (e.key === 'Enter') {
-//         createBoxes(ref.input.valueAsNumber)
-//     } 
-// }
-
-// function destroyBoxes() {
-// ref.boxes.innerHTML = "";
-// }
-
-// ref.create.addEventListener('click', () => createBoxes(ref.input.valueAsNumber));
-// ref.input.addEventListener('keypress', (e) => createBoxesOnEnter(e));
-// ref.clear.addEventListener('click',destroyBoxes);
 
